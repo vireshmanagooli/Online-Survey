ea082be797485a7086e5c792584298d1b7a27e94
Viresh Managooli <viresh.managooli@gmail.com>
2014-05-24 13:23:35 +0530

    Add Customer logic is added

--- OnlineSurvey/src/com/onlinesurvey/service/CustomerService.java
+++ OnlineSurvey/src/com/onlinesurvey/service/CustomerService.java
@@ -3,6 +3,7 @@
  */
 package com.onlinesurvey.service;
 
+import javax.servlet.ServletContext;
 import javax.servlet.http.HttpServletRequest;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
@@ -15,6 +16,9 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import com.onlinesurvey.bean.CustomerBean;
+import com.onlinesurvey.dao.CustomerDAO;
+
 /**
  * @author Viresh
  *
@@ -22,6 +26,9 @@ import org.json.JSONObject;
 @Path("/Customer")
 public class CustomerService {
 
+	@Context
+	private ServletContext context;
+	
 	@Path("/GetCustomers")
 	@POST	
 	@Produces(MediaType.APPLICATION_JSON) 
@@ -61,9 +68,77 @@ public class CustomerService {
 	@POST
 	@Path("/AddCustomers")
 	@Produces(MediaType.APPLICATION_JSON) 
-	public String addProducts(@Context HttpServletRequest request) throws JSONException {
-	
+	public String addProducts(@Context HttpServletRequest request) throws JSONException {			
+		
 		JSONObject returnObject = new JSONObject();
+		String CustomerName = request.getParameter("CustomerName");
+		String CustomerDesc = request.getParameter("CustomerDesc");
+		String CustomerAge = request.getParameter("CustomerAge");
+		String CustomerEmail = request.getParameter("CustomerEmail");
+		String CustomerCell = request.getParameter("CustomerCell");
+		String CustomerDL = request.getParameter("CustomerDL");
+		String CustomerPwd = request.getParameter("CustomerPwd");
+		
+		//Validation
+		
+		String ErrorMessage = null;
+		boolean isValid = true;
+		
+		if(CustomerName == null || CustomerName.isEmpty()){
+			isValid = false;
+			ErrorMessage = "Customer Name Can't be Empty \n";
+		}else if(CustomerDesc == null || CustomerDesc.isEmpty()){
+			isValid = false;
+			ErrorMessage = "Customer Desc Can't be Empty \n";
+		}else if(CustomerAge == null || CustomerAge.isEmpty()){
+			isValid = false;
+			ErrorMessage = "Customer Age Can't be Empty \n";
+		}else if(!isIntegerParseInt(CustomerAge)){
+			isValid = false;
+			ErrorMessage = "Customer Age Can only be Integer \n";
+		}else if(CustomerEmail == null || CustomerEmail.isEmpty()){
+			isValid = false;
+			ErrorMessage = "Customer Email Can't be Empty \n";
+		}else if(!isEmailValid(CustomerEmail)){
+			isValid = false;
+			ErrorMessage = "Email Address is not valid \n";
+		}else if(CustomerCell == null || CustomerCell.isEmpty()){
+			isValid = false;
+			ErrorMessage = "Customer Cell Can't be Empty \n";
+		}else if(!isIntegerParseInt(CustomerCell)){
+			isValid = false;
+			ErrorMessage = "Customer Cell No Can only be Integer \n";
+		}else if(CustomerDL == null || CustomerDL.isEmpty()){
+			isValid = false;
+			ErrorMessage = "Customer DL Can't be Empty \n";
+		}else if(!isValidEmailDL(CustomerDL)){
+			isValid = false;
+			ErrorMessage = "Customer DL Can only be 'North' or 'South' or 'East' or 'West' \n";
+		}else if(CustomerPwd == null || CustomerPwd.isEmpty()){
+			isValid = false;
+			ErrorMessage = "Customer Password Can't be Empty \n";
+		}
+				
+		//Check whether all the validations are passed
+		if(isValid){
+			//Populate the Bean Class then parsist into the database
+			CustomerBean customerBean = new CustomerBean();
+			customerBean.setCustomerAge(Integer.parseInt(CustomerAge));
+			customerBean.setCustomerCell(CustomerCell);
+			customerBean.setCustomerDesc(CustomerDesc);
+			customerBean.setCustomerDL(CustomerDL.toUpperCase());
+			customerBean.setCustomerEmail(CustomerEmail);
+			customerBean.setCustomerName(CustomerName);
+			customerBean.setCustomerPwd(CustomerPwd);
+			
+			new CustomerDAO().addCustomer(customerBean);
+			
+			returnObject.put(ServiceConstants.RESULT, ServiceConstants.OK);
+			returnObject.put(ServiceConstants.RECORDS,customerBean);
+		}else{
+			returnObject.put(ServiceConstants.RESULT, ServiceConstants.ERROR);
+			returnObject.put(ServiceConstants.MESSAGE, ErrorMessage);
+		}
 		
 		System.out.println("AddProducts : " + returnObject.toString());
 		return returnObject.toString();
@@ -89,5 +164,28 @@ public class CustomerService {
 		
 		System.out.println("DeleteProducts : " + returnObject.toString());
 		return returnObject.toString();
+	}
+	
+	private boolean isIntegerParseInt(String str) {
+	     try {
+	           Integer.parseInt(str);
+	            return true;
+	        } catch (NumberFormatException nfe) {}
+	        return false;
+	}
+	
+	private boolean isEmailValid(String email){
+		 String EMAIL_REGEX = "^[\\w-_\\.+]*[\\w-_\\.]\\@([\\w]+\\.)+[\\w]+[\\w]$";			      
+		 return email.matches(EMAIL_REGEX);			      
+	}
+	
+	private boolean isValidEmailDL(String DL){
+		
+		if(DL.equalsIgnoreCase("North") || DL.equalsIgnoreCase("South") ||
+				DL.equalsIgnoreCase("East") || DL.equalsIgnoreCase("West")){
+			return true;
+		}else{
+			return false;
+		}
 	}
 }

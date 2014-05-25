766c47ecd491755ff5d23d6790834fb25da68573
Viresh Managooli <viresh.managooli@gmail.com>
2014-05-25 11:01:37 +0530

    Add Survey Functionality added

--- OnlineSurvey/src/com/onlinesurvey/service/SurveyService.java
+++ OnlineSurvey/src/com/onlinesurvey/service/SurveyService.java
@@ -71,11 +71,20 @@ public class SurveyService {
 	public String createSurvey(@Context HttpServletRequest request) throws JSONException {
 	
 		JSONObject returnObject = new JSONObject();
+					
+		SurveyBean bean = populateBean(request);
 		
-		returnObject.put(ServiceConstants.RESULT, ServiceConstants.ERROR);
-		returnObject.put(ServiceConstants.MESSAGE, "Survey Can't be added from the table. Please use below add survey button");		
+		Long returnId = new SurveyDAO().addSurvey(bean);
 		
-		System.out.println(" : " + returnObject.toString());
+		if(returnId != -1){
+			returnObject.put(ServiceConstants.RESULT, ServiceConstants.OK);
+		}else{
+			
+			returnObject.put(ServiceConstants.RESULT, ServiceConstants.ERROR);
+			returnObject.put(ServiceConstants.MESSAGE, "Survey Can't be added from the table. Please use below add survey button");					
+		}
+		
+		System.out.println("CreateSurvey : " + returnObject.toString());
 		return returnObject.toString();
 	}
 	
@@ -136,5 +145,82 @@ public class SurveyService {
 		returnObj.put("SurveyDesc",surveyBean.getSurveyName());
 		returnObj.put("CustomerDL",surveyBean.getCustomerDL());		
 		return returnObj;
+	}
+	
+	/**
+	 * Method to populate the Bean object
+	 * @param request
+	 * @return
+	 */
+	private SurveyBean populateBean(HttpServletRequest request){
+		SurveyBean bean = new SurveyBean();
+		
+		
+		bean.setSurveyName(request.getParameter("SurveyName"));
+		bean.setSurveyDesc(request.getParameter("SurveyDesc"));
+		bean.setProductId(Long.parseLong(request.getParameter("productName")));
+		bean.setCustomerDL(request.getParameter("CustomerDL"));
+		
+		bean.setQuestion_1(request.getParameter("Question_1"));
+		bean.setOption_1_1(request.getParameter("Option_1_1"));
+		bean.setOption_1_2(request.getParameter("Option_1_2"));
+		bean.setOption_1_3(request.getParameter("Option_1_3"));
+		bean.setOption_1_4(request.getParameter("Option_1_4"));
+		
+		bean.setQuestion_2(request.getParameter("Question_2"));
+		bean.setOption_2_1(request.getParameter("Option_2_1"));
+		bean.setOption_2_2(request.getParameter("Option_2_2"));
+		bean.setOption_2_3(request.getParameter("Option_2_3"));
+		bean.setOption_2_4(request.getParameter("Option_2_4"));
+		
+		bean.setQuestion_3(request.getParameter("Question_3"));
+		bean.setOption_3_1(request.getParameter("Option_3_1"));
+		bean.setOption_3_2(request.getParameter("Option_3_2"));
+		bean.setOption_3_3(request.getParameter("Option_3_3"));
+		bean.setOption_3_4(request.getParameter("Option_3_4"));
+		
+		bean.setQuestion_4(request.getParameter("Question_4"));
+		bean.setOption_4_1(request.getParameter("Option_4_1"));
+		bean.setOption_4_2(request.getParameter("Option_4_2"));
+		bean.setOption_4_3(request.getParameter("Option_4_3"));
+		bean.setOption_4_4(request.getParameter("Option_4_4"));
+		
+		bean.setQuestion_5(request.getParameter("Question_5"));
+		bean.setOption_5_1(request.getParameter("Option_5_1"));
+		bean.setOption_5_2(request.getParameter("Option_5_2"));
+		bean.setOption_5_3(request.getParameter("Option_5_3"));
+		bean.setOption_5_4(request.getParameter("Option_5_4"));
+		
+		bean.setQuestion_6(request.getParameter("Question_6"));
+		bean.setOption_6_1(request.getParameter("Option_6_1"));
+		bean.setOption_6_2(request.getParameter("Option_6_2"));
+		bean.setOption_6_3(request.getParameter("Option_6_3"));
+		bean.setOption_6_4(request.getParameter("Option_6_4"));
+		
+		bean.setQuestion_7(request.getParameter("Question_7"));
+		bean.setOption_7_1(request.getParameter("Option_7_1"));
+		bean.setOption_7_2(request.getParameter("Option_7_2"));
+		bean.setOption_7_3(request.getParameter("Option_7_3"));
+		bean.setOption_7_4(request.getParameter("Option_7_4"));
+		
+		bean.setQuestion_8(request.getParameter("Question_8"));
+		bean.setOption_8_1(request.getParameter("Option_8_1"));
+		bean.setOption_8_2(request.getParameter("Option_8_2"));
+		bean.setOption_8_3(request.getParameter("Option_8_3"));
+		bean.setOption_8_4(request.getParameter("Option_8_4"));
+		
+		bean.setQuestion_9(request.getParameter("Question_9"));
+		bean.setOption_9_1(request.getParameter("Option_9_1"));
+		bean.setOption_9_2(request.getParameter("Option_9_2"));
+		bean.setOption_9_3(request.getParameter("Option_9_3"));
+		bean.setOption_9_4(request.getParameter("Option_9_4"));
+		
+		bean.setQuestion_10(request.getParameter("Question_10"));
+		bean.setOption_10_1(request.getParameter("Option_10_1"));
+		bean.setOption_10_2(request.getParameter("Option_10_2"));
+		bean.setOption_10_3(request.getParameter("Option_10_3"));
+		bean.setOption_10_4(request.getParameter("Option_10_4"));
+		
+		return bean;
 	}
 }

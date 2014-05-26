e0ade5491c8002c852fb4078f235851649d5eab5
Viresh Managooli <viresh.managooli@gmail.com>
2014-05-26 19:01:57 +0530

    Added jTable for user response

--- OnlineSurvey/src/com/onlinesurvey/bean/UserSurveyBean.java
+++ OnlineSurvey/src/com/onlinesurvey/bean/UserSurveyBean.java
@@ -68,4 +68,150 @@ public class UserSurveyBean {
 	
 	@Column(name="Question_10")
 	private String Question_10;
+
+	public Long getUserSurveyId() {
+		return UserSurveyId;
+	}
+
+	public void setUserSurveyId(Long userSurveyId) {
+		UserSurveyId = userSurveyId;
+	}
+
+	public Long getSurveyId() {
+		return SurveyId;
+	}
+
+	public void setSurveyId(Long surveyId) {
+		SurveyId = surveyId;
+	}
+
+	public String getSurveyName() {
+		return SurveyName;
+	}
+
+	public void setSurveyName(String surveyName) {
+		SurveyName = surveyName;
+	}
+
+	public String getSurveyDesc() {
+		return SurveyDesc;
+	}
+
+	public void setSurveyDesc(String surveyDesc) {
+		SurveyDesc = surveyDesc;
+	}
+
+	public String getCustomerName() {
+		return CustomerName;
+	}
+
+	public void setCustomerName(String customerName) {
+		CustomerName = customerName;
+	}
+
+	public String getCustomerDesc() {
+		return CustomerDesc;
+	}
+
+	public void setCustomerDesc(String customerDesc) {
+		CustomerDesc = customerDesc;
+	}
+
+	public String getCustomerEmail() {
+		return CustomerEmail;
+	}
+
+	public void setCustomerEmail(String customerEmail) {
+		CustomerEmail = customerEmail;
+	}
+
+	public String getUserResponse() {
+		return UserResponse;
+	}
+
+	public void setUserResponse(String userResponse) {
+		UserResponse = userResponse;
+	}
+
+	public String getQuestion_1() {
+		return Question_1;
+	}
+
+	public void setQuestion_1(String question_1) {
+		Question_1 = question_1;
+	}
+
+	public String getQuestion_2() {
+		return Question_2;
+	}
+
+	public void setQuestion_2(String question_2) {
+		Question_2 = question_2;
+	}
+
+	public String getQuestion_3() {
+		return Question_3;
+	}
+
+	public void setQuestion_3(String question_3) {
+		Question_3 = question_3;
+	}
+
+	public String getQuestion_4() {
+		return Question_4;
+	}
+
+	public void setQuestion_4(String question_4) {
+		Question_4 = question_4;
+	}
+
+	public String getQuestion_5() {
+		return Question_5;
+	}
+
+	public void setQuestion_5(String question_5) {
+		Question_5 = question_5;
+	}
+
+	public String getQuestion_6() {
+		return Question_6;
+	}
+
+	public void setQuestion_6(String question_6) {
+		Question_6 = question_6;
+	}
+
+	public String getQuestion_7() {
+		return Question_7;
+	}
+
+	public void setQuestion_7(String question_7) {
+		Question_7 = question_7;
+	}
+
+	public String getQuestion_8() {
+		return Question_8;
+	}
+
+	public void setQuestion_8(String question_8) {
+		Question_8 = question_8;
+	}
+
+	public String getQuestion_9() {
+		return Question_9;
+	}
+
+	public void setQuestion_9(String question_9) {
+		Question_9 = question_9;
+	}
+
+	public String getQuestion_10() {
+		return Question_10;
+	}
+
+	public void setQuestion_10(String question_10) {
+		Question_10 = question_10;
+	}
+	
+	
 }
--- OnlineSurvey/src/com/onlinesurvey/service/UserSurveyService.java
+++ OnlineSurvey/src/com/onlinesurvey/service/UserSurveyService.java
@@ -5,9 +5,11 @@ package com.onlinesurvey.service;
 
 import java.util.List;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
+import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
 
 import org.json.JSONArray;
@@ -15,7 +17,9 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 import com.onlinesurvey.bean.SurveyBean;
+import com.onlinesurvey.bean.UserSurveyBean;
 import com.onlinesurvey.dao.SurveyDAO;
+import com.onlinesurvey.dao.UserSurveyDAO;
 
 /**
  * @author Viresh
@@ -24,7 +28,7 @@ import com.onlinesurvey.dao.SurveyDAO;
 @Path("/UserSurvey")
 public class UserSurveyService {
 
-	@Path("/GetUserSurvey")
+	@Path("/GetAllUserSurvey")
 	@POST	
 	@Produces(MediaType.APPLICATION_JSON) 
 	public String getUserSurvey() throws JSONException {
@@ -32,9 +36,33 @@ public class UserSurveyService {
 		JSONObject returnObject = new JSONObject();	
 		JSONArray returnArray = new JSONArray();
 		
-		List<SurveyBean> allSurvey = new SurveyDAO().getAllSurvey();
+		List<UserSurveyBean> allUserSurvey = new UserSurveyDAO().getAllSurvey();
 		
-		return null;
+		for (int i = 0; i < allUserSurvey.size(); i++) {  
+			UserSurveyBean surveyBean = (UserSurveyBean) allUserSurvey.get(i);  
+			JSONObject record = populateJSON(surveyBean);
+			returnArray.put(record);
+		} 		
+					
+		returnObject.put(ServiceConstants.RESULT, ServiceConstants.OK);
+		returnObject.put(ServiceConstants.RECORDS,returnArray);
+		
+		System.out.println("GetProducts : " + returnObject.toString());
+		return returnObject.toString();
+	}
+	
+	@POST
+	@Path("/NoOperation")
+	@Produces(MediaType.APPLICATION_JSON) 
+	public String updateSurvey(@Context HttpServletRequest request) throws JSONException {
+	
+		JSONObject returnObject = new JSONObject();
+		
+		returnObject.put(ServiceConstants.RESULT, ServiceConstants.ERROR);
+		returnObject.put(ServiceConstants.MESSAGE, "Can't Add or Modify or delete user responses");
+		
+		System.out.println(" : " + returnObject.toString());
+		return returnObject.toString();
 	}
 	
 	@Path("/AddUserSurvey")
@@ -61,5 +89,25 @@ public class UserSurveyService {
 		List<SurveyBean> allSurvey = new SurveyDAO().getAllSurvey();
 		
 		return null;
+	}
+	
+	/**
+	 * Populate the Bean object
+	 * @param userSurveyBean
+	 * @return
+	 * @throws JSONException
+	 */
+	private JSONObject populateJSON(UserSurveyBean userSurveyBean) throws JSONException{
+
+		JSONObject returnObj = new JSONObject();
+		returnObj.put("UserSurveyId",userSurveyBean.getUserSurveyId());
+		returnObj.put("SurveyName",userSurveyBean.getSurveyName());
+		returnObj.put("SurveyDesc",userSurveyBean.getSurveyDesc());
+		returnObj.put("CustomerName",userSurveyBean.getCustomerName());	
+		returnObj.put("CustomerDesc",userSurveyBean.getCustomerDesc());	
+		returnObj.put("CustomerEmail",userSurveyBean.getCustomerEmail());	
+		returnObj.put("UserResponse",userSurveyBean.getUserResponse());	
+		
+		return returnObj;
 	}
 }
